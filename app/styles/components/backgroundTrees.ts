7718a68c1562e747917391fe18fffe16764e119c
Shanae Leslie <86532939+ShanaeL29@users.noreply.github.com>
2022-05-11 11:56:13 -0400

    Fixed tree container positioning to appear more distant

--- app/styles/components/backgroundTrees.ts
+++ app/styles/components/backgroundTrees.ts
@@ -1,15 +1,15 @@
 export const backgroundTrees = {
-	c_bgt_middleTree: {
-		marginBottom: -100,
-		marginHorizontal: '-25%',
-		zIndex: 1,
-	},
-	c_bgt_treeContainer: {
-		alignItems: 'center',
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		paddingBottom: 200,
-		position: 'absolute',
-	},
+  c_bgt_middleTree: {
+    marginBottom: -100,
+    marginHorizontal: '-25%',
+    zIndex: 1,
+  },
+  c_bgt_treeContainer: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 400,
+    position: 'absolute',
+  },
 };
