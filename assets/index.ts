76e3bb7e4f9304a49f6719bd62df73886b78a377
Shanae Leslie <86532939+ShanaeL29@users.noreply.github.com>
2022-05-12 14:20:52 -0400

    Added character images to assets

--- assets/index.ts
+++ assets/index.ts
@@ -2,10 +2,12 @@ import { common } from './common';
 import { charSelect } from './charSelectScreen';
 import { startScreen } from './startScreen';
 import { gameOverScreen } from './gameOverScreen';
+import { characters } from './characters';
 
 export const assets = {
   common,
+  characters,
   charSelect,
-  startScreen,
   gameOverScreen,
+  startScreen,
 };
