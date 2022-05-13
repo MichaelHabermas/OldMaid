3a6e51fc16cf9e9206c611b0a7ccd8c3f9c2c672
lailaarkadan <laila.arkadan@gmail.com>
2022-05-13 11:48:19 -0600

    went back to the merge before i merged my branch and broke the main. testing

--- app/styles/screenStyles/gamePlayScreen.ts
+++ app/styles/screenStyles/gamePlayScreen.ts
@@ -1,20 +1,22 @@
 export const gamePlayScreen = {
   gps_options_button: {
-    position: 'absolute',
-    top: '7%',
-    left: '3%',
+    position: "absolute",
+    top: "7%",
+    left: "3%",
   },
   gps_card_container: {
-    width: '18%',
-    height: '12%',
+    width: "18%",
+    height: "12%",
   },
   gps_card: {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
   },
   gps_card_pile: {
-    position: 'absolute',
-    height: '100%',
-    width: '100%',
-  }
-}
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+  },
+};
+
+// testing
